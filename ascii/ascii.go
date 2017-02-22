40641f976e9392e7a562c40f969d52d1ad6f63e0
Christian Moen <cmoen11@gmail.com>
2017-02-22 15:24:12 +0100

    litt mer arbeid fra nenseth

--- ascii/ascii.go
+++ ascii/ascii.go
@@ -19,7 +19,10 @@ const Ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
 // 40 @ 1000000
 // ...
 func IterateOverASCIIStringLiteral(sl string) {
-	// Kode for Oppgave 1a
+
+	for i:= 0; i < len(Ascii); i++ {
+		fmt.Printf("%x %c %c", Ascii[i], Ascii[i], Ascii[i])
+	}
 }
 
 // Funksjonen skal generere en utskrift fra en sekvens av bytes,
