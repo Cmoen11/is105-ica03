9e85be650ca96c38bbfc9f3de0004276ae00cbb1
Christian Moen <cmoen11@gmail.com>
2017-02-22 23:12:33 +0100

    liten fiks

--- ascii/ascii_test.go
+++ ascii/ascii_test.go
@@ -5,16 +5,12 @@ import (
 )
 
 func TestGreetingASCII(t *testing.T) {
-	v := GreetingASCII()
-	for i := 0; i < len(v); i++ {
-		if (isASCII(string(v[i])) == false){
-			// throws error
-			t.Fail()
-			break
-		}
+	if !(isASCII(GreetingASCII())){
+		t.Fail()
 	}
-
 }
+
+
 
 func isASCII(s string) bool {
 	for _, c := range s {
